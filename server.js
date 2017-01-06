b1337ccd53c6597c0d196c47c66240fb8b1662b2
M McWilliams <hi@mmcwilliams.com>
2017-01-05 21:46:27 -0500

    Configure restify
    For standard development and heroku demo

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log('Server started');
+console.log('Server starting...');
+
+var restify = require('restify'),
+	server,
+	port = process.env.PORT || 8080,
+	index;
+
+index = function (req, res, next) {
+	'use strict';
+	res.end('Nothing yet');
+	return next();
+};
+
+server = restify.createServer({
+	name: 'yardsale',
+	version: '0.0.1'
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser({ mapParams: true }));
+server.use(restify.authorizationParser());
+
+//server.get(/\/static\/?.*/, restify.serveStatic({ directory : __dirname }));
+
+server.get('/', index);
+
+server.listen(port, function () {
+	'use strict';
+	console.log('%s listening at %s', server.name, server.url);
+});
