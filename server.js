637dbc4b769bf79d0f526c8bd3bd6e0372a3b33a
mmcwilliams <hi@mmcwilliams.com>
2017-02-04 09:43:28 -0500

    Better library structure, for modularity, and remove test code

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ console.log('Server starting...')
 
 const restify = require('restify')
 const port = process.env.PORT || 8080
-const Views = require('./lib/views.js')
+const Views = require('./lib/views')
 const view = new Views()
 let search
 let server
 let index
 
 if (typeof process.env.AWS_ID !== 'undefined') {
-	search = require('./lib/search.js')
+	search = require('./lib/search')
 }
 
 index = (req, res, next) => {
@@ -35,11 +35,3 @@ server.get('/', index)
 server.listen(port, () => {
 	console.log(`${server.name} listening at ${server.url}`)
 });
-
-search.records({Artist: "Nirvana"}, (err, results, response) => {
-	if (err) {
-		return console.error(err.Error[0].Message)
-	}
-	console.dir(results)
-	console.dir(response)
-})
