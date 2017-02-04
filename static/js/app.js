bcf78b0194aeea219154530c743d570759d6f6a7
mmcwilliams <hi@mmcwilliams.com>
2017-02-04 13:27:49 -0500

    Made whole scene into class

--- static/js/app.js
+++ static/js/app.js
@@ -2,92 +2,111 @@
 /*globals THREE,Stats*/
 
 'use strict'
-const FPS = 24//for animations, not renderer
 
-let WIDTH = window.innerWidth
-let HEIGHT = window.innerHeight
+class YardSale {
+	constructor (selector = '#viewer', debug = true) {
+		this.WIDTH = window.innerWidth
+		this.HEIGHT = window.innerHeight
+		this.ASPECT = this.WIDTH / this.HEIGHT
+		this.VIEW_ANGLE = 45
+		this.NEAR = 0.1
+		this.FAR = 10000
+		this.FPS = 24
+		this.DEBUG = debug
 
-const SCREEN_WIDTH = window.screen.availWidth
-const SCREEN_HEIGHT = window.screen.availHeight
+		this.container = document.querySelector(selector)
+		this.renderer = this.webglAvailable() ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer()
+		this.scene = new THREE.Scene()
 
-const VIEW_ANGLE = 45
-const ASPECT = WIDTH / HEIGHT
-const NEAR = 0.1
-const FAR = 10000
+		this.addCamera()
 
-const RADIUS = 50
-const SEGMENTS = 16
-const RINGS = 16
+		this.renderer.setSize(this.WIDTH, this.HEIGHT)
+		this.container.appendChild(this.renderer.domElement)
 
-function webglAvailable() {
-    try {
-        var c = document.createElement("canvas");
-        return !!window.WebGLRenderingContext && (c.getContext("webgl") || c.getContext("experimental-webgl"))
-    } catch (e) { 
-        return false
-    } 
+		this.addBindings()
+		this.addLights()
+		this.addObjects()
+
+		if (this.DEBUG) {
+			this.addStats()
+		}
+
+		this.update()
+		this.startAnimation()
+	}
+
+	addLights () {
+		const pointLight = new THREE.PointLight(0xFFFFFF)
+		pointLight.position.x = 200
+		pointLight.position.y = 150
+		pointLight.position.z = -300
+
+		const ambLight = new THREE.AmbientLight(0x404040)
+		this.scene.add(ambLight)
+
+		this.scene.add(pointLight)
+	}
+
+	addCamera () {
+		this.camera = new THREE.PerspectiveCamera(this.VIEW_ANGLE, this.ASPECT, this.NEAR, this.FAR )
+		this.camera.position.y = 40
+		this.camera.rotation.x = -0.2
+		this.scene.add(this.camera)
+	}
+
+	addObjects () {
+		const tableMaterial = new THREE.MeshLambertMaterial({ color: 0x996600 })
+		this.table = new THREE.Mesh(new THREE.CubeGeometry(200, 5, 80), tableMaterial)
+		this.table.position.z = -150
+
+		this.scene.add(this.table)
+	}
+
+	addStats () {
+		this.stats = new Stats()
+		this.stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+		document.body.appendChild( this.stats.dom )
+	}
+
+	addBindings () {
+		window.addEventListener( 'resize', this.resize.bind(this), false )
+	}
+
+	update () {
+		this.renderer.render(this.scene, this.camera)
+		if (this.DEBUG) {
+			this.stats.begin()
+			this.stats.end()
+		}
+		requestAnimationFrame( this.update.bind(this) )
+	}
+
+	resize () {
+		this.WIDTH = window.innerWidth
+		this.HEIGHT = window.innerHeight
+		this.ASPECT = this.WIDTH / this.HEIGHT
+
+		this.camera.aspect = this.ASPECT
+    	this.camera.updateProjectionMatrix()
+		this.renderer.setSize( this.WIDTH, this.HEIGHT )
+	}
+
+	animation () {
+		//this.table.rotation.y += 0.01
+	}
+
+	startAnimation () {
+		this.timeout = setInterval(this.animation.bind(this), 1000 / this.FPS)
+	}
+
+	webglAvailable () {
+		try {
+			var c = document.createElement('canvas')
+			return !!window.WebGLRenderingContext && (c.getContext('webgl') || c.getContext('experimental-webgl'))
+		} catch (e) { 
+			return false
+		} 
+	}
 }
 
-const container = document.querySelector('#viewer')
-const renderer = webglAvailable() ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer()
-const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR )
-const scene = new THREE.Scene()
-
-scene.add(camera)
-renderer.setSize(WIDTH, HEIGHT)
-container.appendChild(renderer.domElement)
-
-const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0xCC0000 })
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(RADIUS, SEGMENTS, RINGS), sphereMaterial)
-
-const sM2 = new THREE.MeshLambertMaterial({ color: 0xFFFF99 })
-const s2 = new THREE.Mesh(new THREE.SphereGeometry(2, 8, 8), sM2)
-
-const point = new THREE.Object3D()
-
-s2.position.x = -100
-
-point.add(s2)
-point.position.z = -300
-
-sphere.position.z = -300
-
-scene.add(sphere)
-scene.add(point)
-
-const pointLight = new THREE.PointLight(0xFFFFFF)
-
-pointLight.position.x = 10
-pointLight.position.y = 50
-pointLight.position.z = 130
-
-scene.add(pointLight)
-
-renderer.render(scene, camera)
-
-const stats = new Stats()
-stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom)
-
-/* FUNCTIONS */
-
-function update () {
-    renderer.render(scene, camera)
-
-    stats.begin()
-    stats.end()
-
-    requestAnimationFrame(update)
-}
-
-requestAnimationFrame(update)
-
-function rotatePoint () {
-    point.rotation.y += 0.01
-}
-
-function animations () {
-    rotatePoint()
-}
-
-setInterval(animations, 1000 / FPS)
+const yardsale = new YardSale()
---
+++ static/js/app.old.js
@@ -0,0 +1,93 @@
+/*jshint browser: true*/
+/*globals THREE,Stats*/
+
+'use strict'
+const FPS = 24//for animations, not renderer
+
+let WIDTH = window.innerWidth
+let HEIGHT = window.innerHeight
+
+const SCREEN_WIDTH = window.screen.availWidth
+const SCREEN_HEIGHT = window.screen.availHeight
+
+const VIEW_ANGLE = 45
+const ASPECT = WIDTH / HEIGHT
+const NEAR = 0.1
+const FAR = 10000
+
+const RADIUS = 50
+const SEGMENTS = 16
+const RINGS = 16
+
+function webglAvailable() {
+    try {
+        var c = document.createElement("canvas");
+        return !!window.WebGLRenderingContext && (c.getContext("webgl") || c.getContext("experimental-webgl"))
+    } catch (e) { 
+        return false
+    } 
+}
+
+const container = document.querySelector('#viewer')
+const renderer = webglAvailable() ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer()
+const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR )
+const scene = new THREE.Scene()
+
+scene.add(camera)
+renderer.setSize(WIDTH, HEIGHT)
+container.appendChild(renderer.domElement)
+
+const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0xCC0000 })
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(RADIUS, SEGMENTS, RINGS), sphereMaterial)
+
+const sM2 = new THREE.MeshLambertMaterial({ color: 0xFFFF99 })
+const s2 = new THREE.Mesh(new THREE.SphereGeometry(2, 8, 8), sM2)
+
+const point = new THREE.Object3D()
+
+s2.position.x = -100
+
+point.add(s2)
+point.position.z = -300
+
+sphere.position.z = -300
+
+scene.add(sphere)
+scene.add(point)
+
+const pointLight = new THREE.PointLight(0xFFFFFF)
+
+pointLight.position.x = 10
+pointLight.position.y = 50
+pointLight.position.z = 130
+
+scene.add(pointLight)
+
+renderer.render(scene, camera)
+
+const stats = new Stats()
+stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom)
+
+/* FUNCTIONS */
+
+function update () {
+    renderer.render(scene, camera)
+
+    stats.begin()
+    stats.end()
+
+    requestAnimationFrame(update)
+}
+
+requestAnimationFrame(update)
+
+function rotatePoint () {
+    point.rotation.y += 0.01
+}
+
+function animations () {
+    rotatePoint()
+}
+
+setInterval(animations, 1000 / FPS)
